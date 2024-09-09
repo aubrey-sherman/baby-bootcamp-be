3a897ce5ccc8cbe01274f61480b885befa84bb9a
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-09-09 13:29:00 -0400

    Start routes associated with eating

---
+++ routes/eat.js
@@ -0,0 +1,20 @@
+/** Routes for feeding data. */
+
+import jsonschema from "jsonschema";
+import { Router } from "express";
+
+import { BadRequestError } from "../expressError.js";
+// TODO: import auth for Auth0
+import FeedTimeEntry from "../models/feedTimeEntry.js";
+
+const router = new Router();
+
+/** GET */
+
+/** POST */
+
+/** PATCH */
+
+/** DELETE */
+
+export default router;
