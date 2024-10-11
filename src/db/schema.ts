f54733a206c3120440546957bb0a5c43dd6897c7
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-10-11 15:04:49 -0400

    Add file for Drizzle schema

---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+// TODO: Declare schema here for users and feeding entries
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
+
