fb1345d965c734607a9f1e3b0fde62cfb60c72f3
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-10-17 13:43:44 -0400

    Rename file extension for TS

---
+++ src/app.ts
@@ -0,0 +1,48 @@
+import "dotenv/config";
+import express from 'express';
+import cors from 'cors';
+
+import { NotFoundError } from './expressError.ts';
+import dotenv from 'dotenv';
+import { authenticateJWT } from './middleware/auth.ts';
+import authRoutes from "./routes/auth.ts";
+import feedingRoutes from './routes/feedingEntries.ts';
+import usersRoutes from './routes/users.ts';
+
+// import { User, FeedTimeEntry } from './models/index.js';
+
+dotenv.config();
+
+import morgan from "morgan";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(authenticateJWT);
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/auth", authRoutes);
+app.use("/users", usersRoutes);
+app.use("/feedingEntries", feedingRoutes);
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+  throw new NotFoundError();
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  const status = err.status || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
+
+export default app;
