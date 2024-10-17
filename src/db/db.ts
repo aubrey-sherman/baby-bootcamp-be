4e2cc2db34e84918600e34a3a1a781f8d60dd065
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-10-17 16:08:04 -0400

    Export db

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,9 +4,8 @@ import 'dotenv/config';
 import { getDatabaseUri } from '../../config.ts';
 import { drizzle } from 'drizzle-orm/connect';
 
-const databaseUri = getDatabaseUri();  // Your database URI from the config
+const databaseUri = getDatabaseUri();
 
-async function main() {
-    const db = await drizzle("node-postgres", databaseUri!);
-}
-main();
+const db = await drizzle("node-postgres", databaseUri!);
+
+export default db;
