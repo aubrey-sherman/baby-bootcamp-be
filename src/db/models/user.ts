4b0d1072c21f2e3b484eba663cf58ebb0e257657
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-10-18 18:13:34 -0400

    Modify syntax for queries (columns should be in an object)

--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -1,6 +1,5 @@
 import bcrypt from 'bcrypt';
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
 import { eq } from 'drizzle-orm';
 import { users } from '../schema/users.ts'
 
@@ -10,7 +9,7 @@ import {
   NotFoundError,
   UnauthorizedError,
 } from '../../expressError.ts';
-import { RegisterParams } from '../../types.ts';
+import { tUser } from '../../types.ts';
 import { BCRYPT_WORK_FACTOR } from "../../../config";
 
 /** Related functions for users. */
@@ -26,12 +25,13 @@ class User {
   static async authenticate(username: string, password: string): Promise<User> {
 
     const result = await db
-      .select(
-        users.username,
-        users.password,
-        users.first_name,
-        users.last_name,
-        users.email)
+      .select({
+        username: users.username,
+        password: users.password,
+        first_name: users.first_name,
+        last_name: users.last_name,
+        email: users.email
+  })
       .from(users)
       .where(eq(users.username, username))
       .limit(1)
@@ -42,8 +42,12 @@ class User {
       if (user) {
         const isValidPassword = await bcrypt.compare(password, user.password);
         if (isValidPassword === true) {
-          delete user.password;
-          return user;
+          return {
+            username: user.username,
+            firstName: user.first_name,
+            lastName: user.last_name,
+            email: user.email,
+        };
         }
       }
 
@@ -57,15 +61,16 @@ class User {
    * Throws BadRequestError on duplicates.
    **/
   static async register(
-    { username, password, firstName, lastName, email }: RegisterParams): Promise<User> {
+    { username, password, firstName, lastName, email }: tUser): Promise<User> {
+    console.debug('Backend register function running')
     const duplicateCheck = await db
-      .select(users.username)
+      .select({ username: users.username })
       .from(users)
       .where(eq(users.username, username))
       .limit(1)
       .execute();
 
-    if (duplicateCheck.rows.length > 0) {
+    if (duplicateCheck.length > 0) {
       throw new BadRequestError(`Duplicate username: ${username}`);
     }
 
@@ -81,27 +86,27 @@ class User {
           last_name: lastName,
           email,
         })
-        .returning(
-          users.username,
-          users.first_name,
-          users.last_name,
-          users.email
-        )
+        .returning({
+          username: users.username,
+          first_name: users.first_name,
+          last_name: users.last_name,
+          email: users.email
+        })
         .execute();
 
-    const newUser = newUserResult.rows[0];
+    const newUser = newUserResult[0];
 
     return newUser;
   }
 
   /** Given a username, return data about user.
    *
-   * Returns { username, first_name, last_name, email, feedEntries }
+   * Returns { username, first_name, last_name, email, feedEntries[] }
    *   where feedEntry is { id, volume_in_oz, eliminating, feeding_time, username }
    *
    * Throws NotFoundError if user not found.
    **/
-  static async getUser(username: string) {
+  static async get(username: string): Promise<User> {
     const userRes = await db
       .select(
         users.username,
@@ -119,4 +124,6 @@ class User {
     return user;
   }
 }
+
+export default User;
 
