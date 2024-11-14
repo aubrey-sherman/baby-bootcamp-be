fda785e328db411d840479e8e642b14cd3614133
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-11-14 18:52:19 -0500

    Update types; file needs to be cleaned up

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,68 @@
 // FIXME: rename for consistency
-type tUser = {
+type tFeedingEntryParams = {
+  volumeInOunces: number;
+  eliminating: boolean;
+  feedingTime: Date;
+  username: string;
+}
+
+// interface FeedingBlock {
+//   id: string;
+//   number: number;
+//   isEliminating: boolean;
+//   username: string;
+// }
+// interface UserType {
+//   username: string;
+//   name: string;
+//   email: string;
+//   createdAt: Date;
+//   updatedAt: Date;
+//   feedingBlocks?: FeedingBlock[];
+// }
+// interface UserWithBlocks extends tUser {
+//   feedingBlocks: FeedingBlock[] | undefined;
+// }
+
+// type UserWithBlocks = {
+//   username: string;
+//   firstName: string;
+//   lastName: string;
+//   email: string;
+//   babyName: string;
+//   feedingBlocks: FeedingBlock[];
+// };
+
+// type FeedingBlock = {
+//   id: string;
+//   username: string;
+//   number: number;
+//   isEliminating: boolean;
+// };
+
+type UserType = {
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  babyName: string;
+};
+
+// type UserWithBlocks = UserType & {
+//   feedingBlocks: FeedingBlock[];
+// };
+
+// Define the interface for what a User looks like
+interface IUser {
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  babyName: string;
+}
+
+// Define type for registration parameters
+interface RegisterParams {
   username: string;
   password: string;
   firstName: string;
@@ -8,35 +71,22 @@ type tUser = {
   babyName: string;
 }
 
-// FIXME: rename for consistency
-type tFeedingEntryParams = {
-  volumeInOunces: number;
-  eliminating: boolean;
-  feedingTime: Date;
-  username: string;
-}
-
-interface FeedingBlock {
+type FeedingBlock = {
   id: string;
+  username: string;
   number: number;
   isEliminating: boolean;
-  username: string;
-}
-interface UserType {
-  username: string;
-  name: string;
-  email: string;
-  createdAt: Date;
-  updatedAt: Date;
-  feedingBlocks?: FeedingBlock[];
-}
-interface UserWithBlocks extends tUser {
+};
+
+type UserWithBlocks = IUser & {
   feedingBlocks: FeedingBlock[];
-}
+};
 
 export {
-  tUser,
+  FeedingBlock,
   tFeedingEntryParams ,
   UserType,
-  UserWithBlocks
+  RegisterParams,
+  UserWithBlocks,
+  IUser
 };
