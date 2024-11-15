87659ec396adabbf88f53f6d4f2b981cd5359b15
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-11-15 13:09:04 -0500

    Clean up redundant code

--- src/types.ts
+++ src/types.ts
@@ -1,44 +1,9 @@
-// FIXME: rename for consistency
 type tFeedingEntryParams = {
   volumeInOunces: number;
   eliminating: boolean;
   feedingTime: Date;
   username: string;
 }
-
-// interface FeedingBlock {
-//   id: string;
-//   number: number;
-//   isEliminating: boolean;
-//   username: string;
-// }
-// interface UserType {
-//   username: string;
-//   name: string;
-//   email: string;
-//   createdAt: Date;
-//   updatedAt: Date;
-//   feedingBlocks?: FeedingBlock[];
-// }
-// interface UserWithBlocks extends tUser {
-//   feedingBlocks: FeedingBlock[] | undefined;
-// }
-
-// type UserWithBlocks = {
-//   username: string;
-//   firstName: string;
-//   lastName: string;
-//   email: string;
-//   babyName: string;
-//   feedingBlocks: FeedingBlock[];
-// };
-
-// type FeedingBlock = {
-//   id: string;
-//   username: string;
-//   number: number;
-//   isEliminating: boolean;
-// };
 
 type UserType = {
   username: string;
@@ -48,11 +13,6 @@ type UserType = {
   babyName: string;
 };
 
-// type UserWithBlocks = UserType & {
-//   feedingBlocks: FeedingBlock[];
-// };
-
-// Define the interface for what a User looks like
 interface IUser {
   username: string;
   firstName: string;
@@ -61,7 +21,6 @@ interface IUser {
   babyName: string;
 }
 
-// Define type for registration parameters
 interface RegisterParams {
   username: string;
   password: string;
