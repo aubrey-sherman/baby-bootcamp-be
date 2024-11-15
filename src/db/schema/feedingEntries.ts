eddc688471f616e71abc7a61e6c2ac4343d109db
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-11-15 13:11:59 -0500

    Update schema so data can persist in db while removed from user view in UI

--- src/db/schema/feedingEntries.ts
+++ src/db/schema/feedingEntries.ts
@@ -1,12 +1,13 @@
-import { pgTable, uuid, doublePrecision, timestamp, index } from "drizzle-orm/pg-core";
+import { pgTable, uuid, doublePrecision, timestamp, index, boolean } from "drizzle-orm/pg-core";
 import { sql } from 'drizzle-orm';
 import { feedingBlocks } from "./feedingBlocks.ts";
 
 /** Represents a feeding entry associated with a block. */
 export const feedingEntries = pgTable("feeding_entries", {
   id: uuid('id').primaryKey().default(sql`gen_random_uuid()`),
-  feedingTime: timestamp('feeding_time').notNull(),
+  feedingTime: timestamp('feeding_time', { withTimezone: true }).notNull(),
   volumeInOunces: doublePrecision('volume_in_ounces'),
+  completed: boolean('completed').default(false).notNull(),
   blockId: uuid('block_id')
       .references(() => feedingBlocks.id, {
         onDelete: "cascade", // If a feeding block is deleted, delete its entries
