5e0451893a07c5ec4037c1b70b37a0c1842d13a2
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-12-01 21:16:15 -0500

    Scale back routes for debugging

--- src/app.ts
+++ src/app.ts
@@ -11,77 +11,71 @@ import usersRoutes from './routes/users.js';
 
 dotenv.config();
 
-import morgan from "morgan";
+// import morgan from "morgan";
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Incoming request:', {
-    method: req.method,
-    path: req.path,
-    origin: req.headers.origin
-  });
-  next();
-});
-
 app.use(cors({
   origin: 'https://present-turtles.surge.sh',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-user-timezone'],
   credentials: true
 }));
-
-// After CORS middleware
-app.use((req, res, next) => {
-  console.log('After CORS middleware');
-  next();
-});
 
 app.options('*', cors()); // enable pre-flight for all routes
 
 app.use(express.json());
-app.use(morgan("tiny"));
-app.use(authenticateJWT);
-app.use(express.urlencoded({ extended: true }));
+// app.use(morgan("tiny"));
+// app.use(authenticateJWT);
+// app.use(express.urlencoded({ extended: true }));
 
 app.get("/ping", (req, res) => {
   console.log("Ping route hit");
   res.json({ message: "pong" });
 });
 
-// Routes
-app.use("/auth", authRoutes);
-app.use("/users", usersRoutes);
-app.use("/feeding-routes", feedingRoutes);
-
 app.get("/", (req, res) => {
   console.log("Root route hit");
   res.json({ message: "Server is running" });
 });
 
-app.get("/test", (req, res) => {
-  console.log("Test route hit");
-  res.json({ message: "Test route working" });
-});
+// // Routes
+// app.use("/auth", authRoutes);
+// app.use("/users", usersRoutes);
+// app.use("/feeding-routes", feedingRoutes);
 
-/** Handle 404 errors -- this matches everything */
-app.use(function (req, res, next) {
-  throw new NotFoundError();
-});
+// app.get("/", (req, res) => {
+//   console.log("Root route hit");
+//   res.json({ message: "Server is running" });
+// });
 
-/** Generic error handler; anything unhandled goes here. */
-app.use((
-  err: ExpressError,
-  req: Request,
-  res: Response,
-  next: NextFunction) => {
-  if (process.env.NODE_ENV !== "test") console.error(err.stack);
-  const status = err.status || 500;
-  const message = err.message;
+// app.get("/test", (req, res) => {
+//   console.log("Test route hit");
+//   res.json({ message: "Test route working" });
+// });
 
-  return res.status(status).json({
-    error: { message, status },
-  });
+// /** Handle 404 errors -- this matches everything */
+// app.use(function (req, res, next) {
+//   throw new NotFoundError();
+// });
+
+// /** Generic error handler; anything unhandled goes here. */
+// app.use((
+//   err: ExpressError,
+//   req: Request,
+//   res: Response,
+//   next: NextFunction) => {
+//   if (process.env.NODE_ENV !== "test") console.error(err.stack);
+//   const status = err.status || 500;
+//   const message = err.message;
+
+//   return res.status(status).json({
+//     error: { message, status },
+//   });
+// });
+
+// Basic error handler
+app.use((err: any, req: any, res: any, next: any) => {
+  console.error(err);
+  return res.status(500).json({ error: err.message });
 });
 
 export default app;
