ff2403d223927a2c278c477643d0cdec54ac04e4
Aubrey Sherman <sherman.aubrey@gmail.com>
2025-06-02 13:13:52 -0400

    List latest tests needed for user model

---
+++ src/db/models/user.test.ts
@@ -0,0 +1,15 @@
+
+
+// test that authentication works
+
+// test that Unauthorized error is raised if no matching user
+
+// test that Unauthorized error is raised for wrong password
+
+// test that registration works
+
+// test for Bad Request error with duplicate data
+
+// that that you can get user (needed for successful login)
+
+// Not Found if no such user
