620f5a00bbff7a93e9cac929b35ba80ab3a67a1b
Aubrey Sherman <sherman.aubrey@gmail.com>
2025-06-09 15:34:49 -0400

    Clean up file

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-/* Configuration for application: reads the env variables and exports db URI. */
+/* Reads the env variables and exports db URI. */
 import dotenv from 'dotenv';
 import "colors";
 
@@ -7,13 +7,12 @@ dotenv.config();
 const SECRET_KEY = process.env.SECRET_KEY || "secret-agent-dev";
 const PORT = process.env.PORT || 3001;
 
-// Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-    ? "postgresql:///baby_bootcamp_test"
-    : process.env.DATABASE_URL || "postgresql:///baby_bootcamp";
+    ? "postgresql:///babybootcamp_test"
+    : process.env.DATABASE_URL || "postgresql:///babybootcamp";
 }
-// Speed up bcrypt during tests, since the algorithm safety isn't being tested
+
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 export {
