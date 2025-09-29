c75735a30e9516c20e185be40699046292401097
Aubrey Sherman <sherman.aubrey@gmail.com>
2025-09-29 13:51:07 -0400

    Remove function duplication - import instead

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,21 +1,10 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
-
-function getDatabaseUri() {
-  if (process.env.NODE_ENV === "test") {
-    return "postgresql:///baby_bootcamp_test";
-  }
-
-  if (process.env.NODE_ENV === "production") {
-    return process.env.PRODUCTION_DATABASE_URL || "postgresql:///baby_bootcamp";
-  }
-
-  return process.env.LOCAL_DATABASE_URL || "postgresql:///baby_bootcamp";
-}
+import { getDatabaseUri } from './src/config.ts';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/db/schema',
+  schema: './dist/db/schema',
   dialect: 'postgresql',
   dbCredentials: {
     url: getDatabaseUri(),
