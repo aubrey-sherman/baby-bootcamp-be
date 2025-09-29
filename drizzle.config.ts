4d6e617d3bd57a07c8113a69705d3c5acebf3352
Aubrey Sherman <sherman.aubrey@gmail.com>
2025-09-29 13:23:42 -0400

    Point to different databases based on environment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,24 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+function getDatabaseUri() {
+  if (process.env.NODE_ENV === "test") {
+    return "postgresql:///baby_bootcamp_test";
+  }
+
+  if (process.env.NODE_ENV === "production") {
+    return process.env.PRODUCTION_DATABASE_URL || "postgresql:///baby_bootcamp";
+  }
+
+  return process.env.LOCAL_DATABASE_URL || "postgresql:///baby_bootcamp";
+}
+
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: getDatabaseUri(),
   },
   verbose: true,
   strict: true,
